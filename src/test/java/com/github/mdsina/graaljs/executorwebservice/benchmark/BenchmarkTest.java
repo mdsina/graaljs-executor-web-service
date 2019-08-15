5fa6300808022e83d20936b46b097d1a060f858c
d.s.mikhaylov <d.s.mikhaylov@tinkoff.ru>
2019-08-15 20:02:23 +0700

    Fix benchmark test

--- src/test/java/com/github/mdsina/graaljs/executorwebservice/benchmark/BenchmarkTest.java
+++ src/test/java/com/github/mdsina/graaljs/executorwebservice/benchmark/BenchmarkTest.java
@@ -5,9 +5,9 @@ import com.github.mdsina.graaljs.executorwebservice.execution.JavaScriptSourceEx
 import com.github.mdsina.graaljs.executorwebservice.script.Script;
 import com.github.mdsina.graaljs.executorwebservice.script.ScriptStorageService;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -30,8 +30,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @State(Scope.Benchmark)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
 public class BenchmarkTest {
 
     private static ScriptStorageService scriptStorageService;
@@ -44,16 +44,17 @@ public class BenchmarkTest {
     }
 
     @Test
-//    @Ignore
-    public void executeJmhRunner() throws RunnerException {
+    public void executeJmhRunner() throws RunnerException, IOException {
         String relPath = getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
-        File compilerDir = new File(relPath + "../../target/compiler");
+        File compilerDir = new File(relPath + "../compiler");
+
+        String path = compilerDir.getCanonicalPath();
 
         Options jmhRunnerOptions = new OptionsBuilder()
             // set the class name regex for benchmarks to search for to the current class
             .include("\\." + this.getClass().getSimpleName() + "\\.")
             .warmupIterations(15)
-            .measurementIterations(30)
+            .measurementIterations(10)
             // do not use forking or the benchmark methods will not see references stored within its class
             .forks(0)
             // do not use multiple threads
@@ -66,11 +67,13 @@ public class BenchmarkTest {
             .resultFormat(ResultFormatType.JSON)
 //            .result("/dev/null") // set this to a valid filename if you want reports
             .jvmArgs(
-                "-XX:+UnlockExperimentalVMOptions -XX:+UseJVMCICompiler --module-path="
-                    + compilerDir.getAbsolutePath()
-                    + " --upgrade-module-path="
-                    + compilerDir.getAbsolutePath() + File.separator + "compiler.jar " +
-                    "-server"
+                "-XX:+UnlockExperimentalVMOptions",
+                "-XX:+EnableJVMCI",
+                "-XX:+UseJVMCICompiler",
+                "--module-path=" + path + File.separator + "graal-sdk.jar" + File.pathSeparator
+                    + path + File.separator + "truffle-api.jar",
+                "--upgrade-module-path=" + path + File.separator + "compiler.jar ",
+                "-server"
             )
             .build();
 
