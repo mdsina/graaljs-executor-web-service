e9deeb88cc8ba84857a0315b01098f56cb4c9b58
Daniil Mikhailov <info@mdsina.ru>
2019-08-17 05:48:28 +0700

    Remove unnecessary test

--- src/test/java/com/github/mdsina/graaljs/executorwebservice/ExecutorWebServiceApplicationTests.java
+++ src/test/java/com/github/mdsina/graaljs/executorwebservice/ExecutorWebServiceApplicationTests.java
@@ -10,10 +10,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mdsina.graaljs.executorwebservice.domain.InvocationInfo;
 import com.github.mdsina.graaljs.executorwebservice.domain.Variable;
-import com.github.mdsina.graaljs.executorwebservice.execution.JavaScriptSourceExecutor;
 import com.github.mdsina.graaljs.executorwebservice.execution.JsExecutionResult;
-import com.github.mdsina.graaljs.executorwebservice.script.Script;
-import com.github.mdsina.graaljs.executorwebservice.script.ScriptStorageService;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,11 +30,6 @@ public class ExecutorWebServiceApplicationTests {
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @Autowired
-    private ScriptStorageService scriptStorageService;
-    @Autowired
-    private JavaScriptSourceExecutor javaScriptSourceExecutor;
 
     @Test
     public void checkTransliterateOutput() throws Exception {
@@ -69,29 +61,6 @@ public class ExecutorWebServiceApplicationTests {
         assertNotEquals(null, response.getOutputs().get(0));
         assertEquals("EN_STR", response.getOutputs().get(0).getName());
         assertEquals("Privet, Graal!", response.getOutputs().get(0).getValue());
-    }
-
-    @Test
-    public void perfTest() throws Exception {
-        int ITERATIONS = 100;
-        for (int iter = 0; iter < ITERATIONS; iter++) {
-            long total = 0, start = System.currentTimeMillis(), last = start;
-            for (int i = 1; i < 10_00; i++) {
-                Script script = scriptStorageService.getScript("PERF_1");
-                javaScriptSourceExecutor.execute(
-                    script.getId(),
-                    script.getBody(),
-                    List.of(Variable.builder().name("I").value("PERF_TEST").build())
-                );
-                total += 1;
-                if (i % 1_00 == 0) {
-                    long now = System.currentTimeMillis();
-                    System.out.printf("%d (%d ms)%n", i / 1_00, now - last);
-                    last = now;
-                }
-            }
-            System.out.printf("iter %d: total: %d (%d ms)%n", iter, total, System.currentTimeMillis() - start);
-        }
     }
 }
 
